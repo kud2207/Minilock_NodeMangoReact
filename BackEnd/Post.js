8f7de8ecc8a493a3cff1ada696a47caf81a81510
K22 <ulrichkageu@gmail.com>
2023-12-30 15:46:26 +0100

    Error connection mongo api rest

--- BackEnd/Post.js
+++ BackEnd/Post.js
@@ -28,3 +28,4 @@ const post =[
 ];
 
 module.exports = post
+
--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,8 +1,36 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const data = require("./Post")
+const post = require('./Post')
 const app = express()
 const PORT = 2002
 
+const DB = "mongodb://localhost/blog" 
+const Schema = mongoose.Schema
+
+
+app.listen(PORT, ()=>{console.log(`Se serveur est lancé sur le PORT: ${PORT}`)})
+mongoose.connect(DB, {useNewUrlParser: true  , useUnifiedTopology: true }, () => console.log(`Successfully connected to serveur BD`))
+
+const postSchema = new Schema({
+  title:{
+    type : String,
+    required : true
+  },
+content:{
+    type : String,
+    required : true
+  },
+  createdAt:{
+    type : String,
+    index : true,
+    unique : true
+  },
+  author : String,
+  category: String
+})
+
+const Post = mongoose.model("Post", postSchema)
 
 app.get('/', (req, res) => {
     res.send(`<p>Basic CATHECHDIGITAL</p>
@@ -16,12 +44,19 @@ app.get('/', (req, res) => {
                     </ul>`)
   })
 
-  app.get('/api', (req, res) => {
+  app.get('/api', function (req, res)  {
     res.send('Welcom to the basic API REST CATECHDIGITAL')
   })
 
-  app.get('/api/posts', (req, res) => {
-    res.send(data)
+  app.get('/api/posts/8', function (req, res) {
+    Post.find({}, (error, posts) => {
+      if(error){
+        res.status(400).error(error)
+        return
+      }
+      console.log(posts)
+      res.send(posts)
+    })
   })
   
   app.get('/api/post/:id', (req, res) => {
@@ -30,5 +65,3 @@ app.get('/', (req, res) => {
     res.send(Singlepost[0])
   })
   
-
-app.listen(PORT, ()=>{console.log(`Se serveur est lancé sur le PORT: ${PORT}`)})
